1170927af27cf396dc4044f983994deb916db3ea
Levi <>
2022-05-02 11:55:28 +0200

    BasicChickenAi

---
+++ Assets/Meshtint Free Chicken Mega Toon Series/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] Waypoints;
+    public int currentWaypointGoal;
+    public Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        agent = this.GetComponent<NavMeshAgent>();
+        Waypoints = GameObject.FindGameObjectsWithTag("Chicken Waypoints");
+        Debug.Log("Waypoints = " + Waypoints.Length.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.hasPath == false)
+        {
+            currentWaypointGoal = Random.Range(0, Waypoints.Length + 1);
+            agent.SetDestination(Waypoints[currentWaypointGoal].transform.position);
+            Debug.Log("Moving to Waypoint = " + currentWaypointGoal.ToString());
+        }
+        else
+        {
+            m_Animator.SetBool("Run", true);
+        }
+
+    }
+}
