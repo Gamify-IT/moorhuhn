c811aca5b01e64da3dd0935d26bdc5a34799ca79
Levi <otterbach.levi@gmail.com>
2022-05-04 22:08:57 +0200

    Basic Point Overlay.

--- Assets/AdditionalScripts/GlobalScripts/Global.cs
+++ Assets/AdditionalScripts/GlobalScripts/Global.cs
@@ -6,17 +6,25 @@ public class Global : MonoBehaviour
 {
 
     public GameObject[] answers;
-
+    public int points = 0;
+    public int chickenCount = 5;
     // Update is called once per frame
     void Update()
+    {
+        this.checkChickens();
+        this.updatePoints();
+    }
+
+    void checkChickens()
     {
         this.answers = GameObject.FindGameObjectsWithTag("Answer");
         Debug.Log("There are " + this.answers.Length + " Chickens on the field");
 
-        if (answers.Length < 5)
+        if (answers.Length < chickenCount)
         {
             bool rightAnswerAvailable = false;
-            foreach(GameObject answer in answers){
+            foreach (GameObject answer in answers)
+            {
                 if (answer.GetComponent<TMPro.TextMeshProUGUI>().text.Equals("Answer"))
                 {
                     rightAnswerAvailable = true;
@@ -24,12 +32,25 @@ public class Global : MonoBehaviour
             }
             if (rightAnswerAvailable)
             {
+                this.points--;
                 Debug.Log("YOU KILLTED THE WRONG CHICKEN, FKKKKKKKK");
             }
             else
             {
+                this.points++;
                 Debug.Log("YOU KILLTED THE RIGHT CHICKEN, YIPPPPPPI");
             }
+            this.chickenCount--;
         }
+    }
+
+    /**
+     * This Method updates the Point counter 
+     * in the top left of the Screen.
+     */
+    void updatePoints()
+    {
+        GameObject pointOverlay = GameObject.FindGameObjectWithTag("Point Overlay");
+        pointOverlay.GetComponent<TMPro.TextMeshProUGUI>().text = ""+this.points;
     }
 }
